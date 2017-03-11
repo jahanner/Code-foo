21237fd1b45546f35f28fe59bcf11179d50b09ef
Jesse Hanner <jahanner7@gmail.com>
2017-03-11 15:47:53 -0600

    Always need to with my RAM

--- frontEnd.js
+++ frontEnd.js
@@ -1,26 +1,31 @@
 
-    function apiCaller(type) {
-        var url = 'http://ign-apis.herokuapp.com/' + type + '?startIndex=0&count=10';
-        $.ajax({
-            url: url,
-            // The name of the callback parameter as specified by the YQL service
-            jsonp: "callback",
-            //Tell jQuery we're expection jsonp
-            dataType: 'jsonp',
-            //Tell YQL what we want and that we want JSON
-            data: {
-                format: 'json'
-            },
-            success: function(response) {
-                for (var i = 0; i < response.data.length; i++){
-                   rowDiv = document.createElement("div");
-                   rowDiv.id = "content" + i;
-                   if (type === "articles") {rowDiv.innerHTML = "<p> <h3>"+response.data[i].metadata.headline+"</h3></p><p> "+response.data[i].metadata.subHeadline+"</p>";
-                    }
-               else {
-                   rowDiv.innerHTML = "<p> <h3>"+response.data[i].metadata.name+"</h3></p><p> "+response.data[i].metadata.description+"</p>";
-                    }
-                document.getElementById("table").appendChild(rowDiv);
+function apiCaller(type) {
+    var url = 'http://ign-apis.herokuapp.com/' + type + '?startIndex=0&count=10';
+    $.ajax({
+        url: url,
+        // The name of the callback parameter as specified by the YQL service
+        jsonp: "callback",
+        //Tell jQuery we're expection jsonp
+        dataType: 'jsonp',
+        //Tell YQL what we want and that we want JSON
+        data: {
+            format: 'json'
+        },
+        success: function(response) {
+            var myNode = document.getElementById("divDump");
+            while(myNode.lastChild) {
+                myNode.removeChild(myNode.lastChild);
+            }
+
+            for (var i = 0; i < response.data.length; i++){
+               rowDiv = document.createElement("div");
+               rowDiv.id = "content" + i;
+               if (type === "articles") {rowDiv.innerHTML = "<p> <h3>"+response.data[i].metadata.headline+"</h3></p><p> "+response.data[i].metadata.subHeadline+"</p>";
+                }
+           else {
+               rowDiv.innerHTML = "<p> <h3>"+response.data[i].metadata.name+"</h3></p><p> "+response.data[i].metadata.description+"</p>";
+                }
+            document.getElementById("divDump").appendChild(rowDiv);
             }
         }
     });
